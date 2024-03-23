3cddbc1d52c01c3f0c1425c0459df4a9e476c7bf
Franklin Almeida Campos <franklindrw@gmail.com>
2024-03-22 23:15:44 -0300

    chore: adicionando toolbar component

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -14,6 +14,7 @@ import { ChartModule } from 'primeng/chart'
 
 import { MessageService } from 'primeng/api';
 import { CookieService } from 'ngx-cookie-service';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -30,6 +31,7 @@ import { CookieService } from 'ngx-cookie-service';
     CardModule,
     ToastModule,
     ChartModule,
+    SharedModule,
   ],
   providers: [MessageService, CookieService]
 })
